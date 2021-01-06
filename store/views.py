d8a5a76009af59b3b7493cbee5b61fe360b19ff6
swatantra94 <swatantranigam9451@gmail.com>
2021-01-06 22:01:54 +0530

    add to cart funnctionality added using with ajax

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
+import json
 
 
 def store(request):
@@ -28,3 +30,23 @@ def checkout(request):
         order = {'get_cart_total':0,'get_cart_items':0}
     context = {'items':items,'order':order}
     return render(request,'store/checkout.html',context)
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('productId:',productId)
+    print('action:',action)
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer,complete = False)
+    orderItem,created = OrderItem.objects.get_or_create(order=order,product=product)
+    if action=='add':
+        orderItem.quantity = orderItem.quantity+1
+    elif action=='remove':
+        orderItem.quantity = orderItem.quantity-1
+    orderItem.save()
+    if orderItem.quantity<=0:
+        orderItem.delete()
+
+    return JsonResponse('item added in cart',safe=False)
